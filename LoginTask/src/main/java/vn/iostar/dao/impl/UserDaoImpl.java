7185c3cbafd884ca4217e0601e32c44f687b78dc
unknown <nhunguyetpy206@gmail.com>
2024-09-19 17:29:49 +0700

    third commit : register view

--- LoginTask/src/main/java/vn/iostar/dao/impl/UserDaoImpl.java
+++ LoginTask/src/main/java/vn/iostar/dao/impl/UserDaoImpl.java
@@ -109,8 +109,78 @@ public class UserDaoImpl extends DBConnection implements IUserDao {
 		
 		return null;
 	}
+	@Override
+	public boolean checkExistEmail(String email) {
+		boolean duplicate = false;
+		String query = "select * from users where email = ?";
+		try {
+			conn = super.getDatabaseConnection();
+			ps = conn.prepareStatement(query);
+			ps.setString(1, email);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+			duplicate = true;
+			}
+			ps.close();
+			conn.close();
+		} catch (Exception ex) {}
+		return duplicate;
+	}
 
+	@Override
+	public boolean checkExistUsername(String username) {
+		boolean duplicate = false;
+		String query = "select * from users where username = ?";
+		try {
+			conn = super.getDatabaseConnection();
+			ps = conn.prepareStatement(query);
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+			duplicate = true;
+			}
+			ps.close();
+			conn.close();
+		} catch (Exception ex) {}
+		return duplicate;
+	}
 
+	@Override
+	public boolean checkExistPhone(String phone) {
+		boolean duplicate = false;
+		String query = "select * from users where phone = ?";
+		try {
+			conn = super.getDatabaseConnection();
+			ps = conn.prepareStatement(query);
+			ps.setString(1, phone);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+			duplicate = true;
+			}
+			ps.close();
+			conn.close();
+		} catch (Exception ex) {}
+		return duplicate;
+	}
 
+	@Override
+	public void insert(UserModel user) {
+		
+		String sql = "INSERT INTO users(email,userName,fullName,passWord,avatar,roleid,phone,createdDate) VALUES(?,?,?,?,?,?,?,?)";
+		try {
+			conn = super.getDatabaseConnection();
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getEmail());
+			ps.setString(2, user.getUserName());
+			ps.setString(3, user.getFullName());
+			ps.setString(4, user.getPassWord());
+			ps.setString(5, user.getAvatar());
+			ps.setInt(6,user.getRoleid());
+			ps.setString(7,user.getPhone());
+			ps.setDate(8, user.getCreatedDate());
+			ps.executeUpdate();
+		} catch (Exception e) {e.printStackTrace();}
+		
+	}
 
 }
--- LoginTask/src/main/java/vn/iostar/services/impl/UserServiceImpl.java
+++ LoginTask/src/main/java/vn/iostar/services/impl/UserServiceImpl.java
@@ -44,4 +44,37 @@ public class UserServiceImpl implements IUserService {
 		}
 		
 	}
+	@Override
+	public void insert(UserModel user) {
+		userDao.insert(user);
+	}
+
+	@Override
+	public boolean register(String username, String password, String email, String fullname, String phone) {
+		if (userDao.checkExistUsername(username)) {
+			return false;
+			}
+			long millis=System.currentTimeMillis();
+			java.sql.Date date=new java.sql.Date(millis);
+			userDao.insert(new UserModel(email, username, fullname,password,
+			null,5,phone,date));
+			return true;
+	}
+
+	@Override
+	public boolean checkExistEmail(String email) {
+		return userDao.checkExistEmail(email);	
+	}
+
+	@Override
+	public boolean checkExistUsername(String username) {
+		return userDao.checkExistUsername(username);
+	}
+
+	@Override
+	public boolean checkExistPhone(String phone) {
+		return userDao.checkExistPhone(phone);
+		
+	}
+
 }
